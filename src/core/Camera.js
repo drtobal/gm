d2f633bd6c158d4297940021d0cc4f8b147fdcd5
Cristóbal Díaz Álvarez <cristobal@cristobaldiaz.cl>
2015-05-24 16:36:46 -0400

    feat: activa transparencia para los colliders que se interponen entre la cámara y el mesh del actor principal

--- src/core/Camera.js
+++ src/core/Camera.js
@@ -29,15 +29,14 @@ GM.Camera = new function () {
         me.camera.rotation.y = -Math.PI / 4;
         me.camera.rotation.x = Math.atan(-1 / Math.sqrt(2));
         buildControls();
+        GM.beforeRender.add("GM.Camera.makeMeshTransparent", makeMeshTransparent);
     };
 
-    /**
+    /*
      * Crea los controles que definirán el comportamiento de la cámara en la
      * aplicación
-     * @method GM.Camera.buildControls
-     * @private
      */
-    var buildControls = function () {
+    function buildControls() {
         me.controls = new THREE.OrbitControls(me.camera, GM.Renderer.renderer.domElement);
         me.controls.addEventListener("change", GM.Renderer.render);
         me.controls.noZoom = false;
@@ -46,5 +45,66 @@ GM.Camera = new function () {
         me.controls.noPan = true;
         me.controls.minPolarAngle = Math.PI / 5;
         me.controls.maxPolarAngle = Math.PI / 2;
-    };
+    }
+
+    /*
+     * Activa transparencia para los meshes que se interpongan entre la cámara y el
+     * usuario, esta función se enlaca al proceso de renderizado en {@link GM.Camera.build}
+     */
+    function makeMeshTransparent() {
+        var vector = new THREE.Vector3();
+        var ray = new THREE.Raycaster();
+        var dir = new THREE.Vector3();
+
+        if (me.camera instanceof THREE.OrthographicCamera) {
+            vector.set(0, 0, -1);
+            vector.unproject(me.camera);
+            dir.set(0, 0, -1).transformDirection(me.camera.matrixWorld);
+            ray.set(vector, dir);
+        } else if (me.camera instanceof THREE.PerspectiveCamera) {
+            vector.set(0, 0, 0.5);
+            vector.unproject(me.camera);
+            ray.set(me.camera.position, vector.sub(me.camera.position).normalize());
+        }
+        var custom = GM.World.colliders.slice(0);
+        custom.push(GM.mainActor.actor.mesh);
+        var intersects = ray.intersectObjects(custom, true);
+        GM.World.colliders.forEach(function (obj) {
+            if (obj instanceof THREE.Mesh) {
+                if (obj.material instanceof THREE.MeshFaceMaterial)
+                    obj.material.materials.forEach(function (mat) {
+                        mat.opacity = 1;
+                    });
+                else
+                    obj.material.opacity = 1;
+            } else {
+                obj.children.forEach(function (chl) {
+                    if (chl.material instanceof THREE.MeshFaceMaterial)
+                        chl.material.materials.forEach(function (mat) {
+                            mat.opacity = 1;
+                        });
+                    else
+                        chl.material.opacity = 1;
+                });
+            }
+        });
+        if (intersects.length > 0) {
+            var actor = GM.mainActor.actor.mesh;
+            var distance = Math.sqrt(
+                    Math.pow(actor.position.x - me.camera.position.x, 2) +
+                    Math.pow(actor.position.y - me.camera.position.y, 2) +
+                    Math.pow(actor.position.z - me.camera.position.z, 2));
+            intersects.forEach(function (intersect) {
+                if (intersect.distance < distance) {
+                    if (intersect.object.material instanceof THREE.MeshFaceMaterial) {
+                        intersect.object.material.materials.forEach(function (mat) {
+                            mat.opacity = .2;
+                        });
+                    } else {
+                        intersect.object.material.opacity = .2;
+                    }
+                }
+            });
+        }
+    }
 };
